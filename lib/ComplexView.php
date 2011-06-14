aab81274b54f2fa1c02aa9903b63dea85c7c6f7a
Kim A. Betti <kim.betti@gmail.com>
2011-06-15 00:35:24 +0200

    Replaced tabs with spaces

--- lib/ComplexView.php
+++ lib/ComplexView.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once dirname(__FILE__) . '/View.php';
 
 /**
@@ -8,32 +9,32 @@ require_once dirname(__FILE__) . '/View.php';
  */
 class ComplexView extends View {
 
-	private $views = array();
+    private $views = array();
 
-	/**
+    /**
      * Construct a view based on a template path. 
      * @param string $viewFile Path to template file
      */
-	public function __construct($layoutFile) {
-		parent::__construct($layoutFile);
-	}
+    public function __construct($layoutFile) {
+        parent::__construct($layoutFile);
+    }
 
     /**
      * Add a view using a given name. 
      * @param string $name Name of the view used in the layout
      * @param View $view The instance to be associated with the name
      */
-	public function addView($name, View $view) {
-		$this->views[$name] = $view;
-	}
+    public function addView($name, View $view) {
+        $this->views[$name] = $view;
+    }
 
-	public function render() {
-		foreach ($this->views as $viewName => $view) {
-			$view->set("layout", $this);
-			$this->vars[$viewName] = $view->render();
-		}
-        
-		return parent::render();
-	}
+    public function render() {
+        foreach ($this->views as $viewName => $view) {
+            $view->set("layout", $this);
+            $this->vars[$viewName] = $view->render();
+        }
+
+        return parent::render();
+    }
 
 }
